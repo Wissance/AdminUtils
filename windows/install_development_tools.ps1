9cfb8cfff41c0c235ad65e5c4817f6334387a166
Michael Ushakov <um.nix.user@gmail.com>
2021-04-30 15:23:49 +0500

    common part removed

--- windows/install_development_tools.ps1
+++ windows/install_development_tools.ps1
@@ -1,11 +1,6 @@
 # set execution policy that allows to download & install
+db_password = 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# install common packages
-choco install chocolateygui -y
-choco install 7zip -y
-choco install adobereader -y
-choco install firefox -y
-choco install googlechrome -y --ignore-checksums
 # install development software: python, go, ruby
 choco install golang -y
 choco install python -y
