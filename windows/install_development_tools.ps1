2ad3e404b7c41bc187a5e9398c07101975abec87
Michael Ushakov <um.nix.user@gmail.com>
2021-05-01 00:20:42 +0500

    passing password param to script

--- windows/install_development_tools.ps1
+++ windows/install_development_tools.ps1
@@ -1,12 +1,15 @@
 # set execution policy that allows to download & install
-db_password = 
+db_password = $args[0]
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # install development software: python, go, ruby
 choco install golang -y
 choco install python -y
 choco install ruby -y
 # install database server
-choco install postgresql13 --params '/Password:123' -y
+choco install postgresql13 --params '/Password:$db_password' -y
 choco install pgadmin4 -y
+choco install mysql -y
+choco install mysql.workbench -y
+choco install sqlite -y
 # install other tools
 choco install nssm -y
