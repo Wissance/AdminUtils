36aa55cccd6342cc940e4bf49de59ba7462d6f24
Michael Ushakov <um.nix.user@gmail.com>
2021-05-04 20:25:38 +0500

    + header

--- windows/install_common_tools.ps1
+++ windows/install_common_tools.ps1
@@ -1,3 +1,5 @@
+# This script was developed by Wissance LLC (M. Ushakov)
+# It must be running in Adminstarotor mode
 # set execution policy that allows to download & install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # install common packages
