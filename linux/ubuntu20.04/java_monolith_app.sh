889a0399f2e17f6dc00dcd90a789da8acb48a895
Ushakov Michael <um.nix.user@gmail.com>
2022-12-06 22:08:05 +0500

    added options

--- linux/ubuntu20.04/java_monolith_app.sh
+++ linux/ubuntu20.04/java_monolith_app.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+# This script MUST be run with sudo
+########################################### VARIABLES #####################################
 JAVA_VERSION="18"
 JAVA_PKG="openjdk-$JAVA_VERSION-jdk"
 NODE_VERSION="16.x"
 NODE_URL="https://deb.nodesource.com/setup_$NODE_VERSION"
-# This script should be run with sudo
+###########################################################################################
 # 
 echo "*************** Welcome to Wissance (https://wissance.com) pure server installation for monolith Java app ***************"
 echo "###################################################################################"
@@ -26,3 +28,20 @@ echo "##########################################################################
 echo "*************** DB Server: Postgresql installation started ***************"
 apt-get install -y postgresql postgresql-contrib
 echo "*************** DB Server: Postgresql installation finished ***************"
+# 4. Install Depending services
+# 4.1 Nginx
+echo "*************** Reverse proxy: Postgresql installation started ***************"
+apt-get install -y nginx
+# 4.2 KeyCloak
+wget https://github.com/keycloak/keycloak/releases/download/19.0.3/keycloak-19.0.3.tar.gz
+tar -xf keycloak-19.0.3.tar.gz
+# 4.3 Elasticsearch
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+apt-get update
+apt-get install elasticsearch
+systemctl enable elasticsearch
+systemctl start elasticsearch
+# 4.4 Certbot
+# 5. Configure installed servers
+# 5.1 PostgresSQL
+echo "*************** Please don't forget to give us a STAR on github: https://github.com/Wissance/AdminUtils and FOLLOW our organization ***************"
