610cc163a89ff8d0cb68b378ac30319ab20032f4
Ushakov Michael <um.nix.user@gmail.com>
2022-12-09 15:21:06 +0500

    intermediate keep working on install script

--- linux/ubuntu20.04/java_monolith_app.sh
+++ linux/ubuntu20.04/java_monolith_app.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+﻿#!/bin/bash
+# IMPORTANT NOTES: If you edit in any text editor ensure that ALL lines ends witn \n (LF) NOT a \r\n (CRLF),  
+# otherwise it won't work!
 # This script MUST be run with sudo
-# args: $1 - keycloak install dir
+# args: $1 - download install dir
+# OS: Ubuntu 22.04 
 ########################################### VARIABLES #####################################
 JAVA_VERSION="18"
 JAVA_PKG="openjdk-$JAVA_VERSION-jdk"
@@ -10,13 +13,13 @@ KEYCLOAK_INSTALL_DIR=$1
 KEYCLOAK_VERSION="19.0.3"
 KEYCLOAK_DOWNLOAD_URL="https://github.com/keycloak/keycloak/releases/download/$KEYCLOAK_VERSION/keycloak-$KEYCLOAK_VERSION.tar.gz"
 ###########################################################################################
-# 
 echo "*************** Welcome to Wissance (https://wissance.com) pure server installation for monolith Java app ***************"
 echo "###################################################################################"
 # 1. Install Backend SDK Open JDK 18
 echo "*************** Backend: Java Development Kit installation started"
 # todo(umv): think about java version parametrization
-add-apt-repository ppa:openjdk-r/ppa
+add-apt-repository -y ppa:openjdk-r/ppa
+echo "installing following JDK package(s): $JAVA_PKG"
 apt-get update
 apt-get install -y $JAVA_PKG
 echo "*************** Backend: Java Development Kit installation finished ***************"
@@ -30,33 +33,33 @@ echo "*************** Frontend: NodeJS && Npm installation finished ************
 echo "###################################################################################"
 # 3. Install PosgtreSQL
 echo "*************** DB Server: Postgresql installation started ***************"
-apt-get install -y postgresql postgresql-contrib
+#apt-get install -y postgresql postgresql-contrib
 echo "*************** DB Server: Postgresql installation finished ***************"
 # 4. Install Depending services
 # 4.1 Nginx
 echo "*************** Reverse proxy: Postgresql installation started ***************"
-apt-get install -y nginx
+#apt-get install -y nginx
 # 4.2 KeyCloak
-if [ -z "$KEYCLOAK_INSTALL_DIR" ]
-then
-    $KEYCLOAK_INSTALL_DIR="/usr/local/sbin/keycloak"
-fi
-mkdir -p $KEYCLOAK_INSTALL_DIR
-cd $KEYCLOAK_INSTALL_DIR
-wget $KEYCLOAK_DOWNLOAD_URL
-tar -xf keycloak-19.0.3.tar.gz
+#if [ -z "$KEYCLOAK_INSTALL_DIR" ] 
+#then
+#    KEYCLOAK_INSTALL_DIR="/usr/local/sbin/keycloak"
+#fi
+#mkdir -p $KEYCLOAK_INSTALL_DIR
+#"cd $KEYCLOAK_INSTALL_DIR || return"
+#wget $KEYCLOAK_DOWNLOAD_URL
+#tar -xf keycloak-19.0.3.tar.gz
 # 4.3 Elasticsearch
-curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-apt-get update
-apt-get install elasticsearch
-systemctl enable elasticsearch
-systemctl start elasticsearch
+#curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+#apt-get update
+#apt-get install elasticsearch
+#systemctl enable elasticsearch
+#systemctl start elasticsearch
 # 4.4 Certbot
-snap install core; sudo snap refresh core
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
-snap set certbot trust-plugin-with-root=ok
-snap install certbot-dns-digitalocean
+#snap install core; sudo snap refresh core
+#snap install --classic certbot
+#ln -s /snap/bin/certbot /usr/bin/certbot
+#snap set certbot trust-plugin-with-root=ok
+#snap install certbot-dns-digitalocean
 # 5. Configure installed servers
 # 5.1 PostgresSQL password 4 postgre
 # 5.2 Minimal 4 kc (user, certs, & so on ....)
